32b113b79b82d979ea999457a4d48b46557700df
LuongQuocThien <73396125+LuongQuocThien@users.noreply.github.com>
2021-04-11 21:01:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
+
+b='\033[1m'
+r='\E[31m'
+g='\E[32m'
+c='\E[36m'
+endc='\E[0m'
+enda='\033[0m'
+
 printf "\n\n$c$b    Loading Installer $endc$enda" >&2
 {
     sudo add-apt-repository ppa:obsproject/obs-studio -y
@@ -25,6 +33,7 @@ printf "$g$b    Installing Desktop Environment $endc$enda" >&2
     sudo apt-get install xfce4-terminal -y
     sudo DEBIAN_FRONTEND=noninteractive \
         apt install --assume-yes xfce4 desktop-base
+        apt remove --assume-yes gnome-terminal
     sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
     sudo apt install --assume-yes xscreensaver
     sudo systemctl disable lightdm.service
@@ -42,38 +51,3 @@ printf "$g$b    Installing Google Chrome $endc$enda" >&2
 } &> /dev/null &&
 printf "\r$c$b    Google Chrome Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-# Install OBS Studio
-printf "$g$b    Installing OBS Studio $endc$enda" >&2
-{
-    sudo apt install ffmpeg -y
-    sudo add-apt-repository ppa:obsproject/obs-studio -y
-    sudo apt install obs-studio -y
-    sudo apt install --assume-yes --fix-broken
-} &> /dev/null &&
-printf "\r$c$b    OBS Studio Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-# Install VLC Media Player 
-printf "$g$b    Installing VLC Media Player $endc$enda" >&2
-{
-    sudo apt install vlc -y
-} &> /dev/null &&
-printf "\r$c$b    VLC Media Player Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-# Install other tools
-printf "$g$b    Installing Other Tools $endc$enda" >&2
-{
-    sudo apt install nautilus nano -y
-    sudo apt-get install gdebi -y
-    sudo apt-get install vim -y
-    sudo add-apt-repository --yes ppa:ethereum/ethereum 
-    sudo apt update && sudo apt-get update
-    sudo apt-get install build-essential cmake libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev -y
-    sudo apt install ethereum -y
-} &> /dev/null &&
-printf "\r$c$b    Other Tools Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-printf "\n$g$b    Installation Completed $endc$enda\n\n" >&2
